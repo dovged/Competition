12482e03e0b939680b9fbba50a34cc418cdb554e
dovged <dovilegedmin@gmail.com>
2018-03-16 20:35:43 +0200

    Route controllers update

---
+++ Competition/Controllers/RouteClimController.cs
@@ -0,0 +1,82 @@
+﻿using Competition.Context;
+using Competition.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Competition.Controllers
+{
+    [RoutePrefix("/api/competition/id/routeClim")]
+    public class RouteClimController : BaseAPIController
+    {
+        /** Grąžina visas vienų varžybų laipiojimo trasas*/
+        public HttpResponseMessage Get(int compId)
+        {
+            if (CompetitionDB.TblCompetitions.FirstOrDefault(x => x.Id == compId) != null)
+            {
+                if (CompetitionDB.TblRoutesClim.ToArray().Where(x => x.CompetitionId == compId).Select(x => new RouteClimbModel(x)).ToList() != null)
+                {
+                    return ToJsonOK(CompetitionDB.TblRoutesClim.ToArray().Where(x => x.CompetitionId == compId).Select(x => new RouteClimbModel(x)).ToList());
+                }
+
+                return ToJsonNotFound("Tuščias sąrašas.");
+            }
+
+            return ToJsonNotFound("Tuščias sąrašas.");
+        }
+
+        /** Grąžina vienų varžybų vieną laipiojimo trasą*/
+        public HttpResponseMessage Get(int compId, int routeId)
+        {
+            if (CompetitionDB.TblCompetitions.FirstOrDefault(x => x.Id == compId) != null)
+            {
+                if (CompetitionDB.TblRoutesClim.FirstOrDefault(x => x.Id == routeId) != null)
+                {
+                    return ToJsonOK(CompetitionDB.TblRoutesClim.FirstOrDefault(x => x.Id == routeId));
+                }
+
+                return ToJsonNotFound("Objektas nerastas.");
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+
+        /** Sukuria naują laipiojimo trasos objektą*/
+        public HttpResponseMessage Post([FromBody]TblRouteClimb value)
+        {
+            CompetitionDB.TblRoutesClim.Add(value);
+            CompetitionDB.SaveChanges();
+            return ToJsonCreated(value);
+        }
+
+        /** Redaguojama viena laipiojimo trasa*/
+        public HttpResponseMessage Put(int routeId, [FromBody]TblRouteClimb value)
+        {
+            if (CompetitionDB.TblRoutesClim.FirstOrDefault(x => x.Id == routeId) != null)
+            {
+                CompetitionDB.Entry(value).State = EntityState.Modified;
+                CompetitionDB.SaveChanges();
+                return ToJsonOK(value);
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+
+        /** Ištrinti vienos laipiojimo trasos duomenis*/
+        public HttpResponseMessage Delete(int id)
+        {
+            if (CompetitionDB.TblRoutesClim.FirstOrDefault(x => x.Id == id) != null)
+            {
+                CompetitionDB.TblRoutesClim.Remove(CompetitionDB.TblRoutesClim.FirstOrDefault(x => x.Id == id));
+                CompetitionDB.SaveChanges();
+                return ToJsonOK("Objektas ištrintas.");
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+    }
+}
---
+++ Competition/Controllers/RouteKKTController.cs
@@ -0,0 +1,82 @@
+﻿using Competition.Context;
+using Competition.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Competition.Controllers
+{
+    [RoutePrefix("/api/competition/id/routeKKT/")]
+    public class RouteKKTController : BaseAPIController
+    {
+        /** Grąžina visas vienų varžybų KKT trasas*/
+        public HttpResponseMessage Get(int compId)
+        {
+            if (CompetitionDB.TblCompetitions.FirstOrDefault(x => x.Id == compId) != null)
+            {
+                if (CompetitionDB.TblRoutesKKT.ToArray().Where(x => x.CompetitionId == compId).Select(x => new RouteKKTModel(x)).ToList() != null)
+                {
+                    return ToJsonOK(CompetitionDB.TblRoutesKKT.ToArray().Where(x => x.CompetitionId == compId).Select(x => new RouteKKTModel(x)).ToList());
+                }
+
+                return ToJsonNotFound("Tuščias sąrašas.");
+            }
+
+            return ToJsonNotFound("Tuščias sąrašas.");
+        }
+
+        /** Grąžina vienų varžybų vieną KKT trasą*/
+        public HttpResponseMessage Get(int compId, int routeId)
+        {
+            if (CompetitionDB.TblCompetitions.FirstOrDefault(x => x.Id == compId) != null)
+            {
+                if (CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == routeId) != null)
+                {
+                    return ToJsonOK(CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == routeId));
+                }
+
+                return ToJsonNotFound("Objektas nerastas.");
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+
+        /** Sukuria naują KKT trasos objektą*/
+        public HttpResponseMessage Post([FromBody]TblRouteKKT value)
+        {
+            CompetitionDB.TblRoutesKKT.Add(value);
+            CompetitionDB.SaveChanges();
+            return ToJsonCreated(value);
+        }
+
+        /** Redaguojama viena KKT trasa*/
+        public HttpResponseMessage Put(int routeId, [FromBody]TblRouteKKT value)
+        {
+            if (CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == routeId) != null)
+            {
+                CompetitionDB.Entry(value).State = EntityState.Modified;
+                CompetitionDB.SaveChanges();
+                return ToJsonOK(value);
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+
+        /** Ištrinti vienos KKT trasos duomenis*/
+        public HttpResponseMessage Delete(int id)
+        {
+            if (CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == id) != null)
+            {
+                CompetitionDB.TblRoutesKKT.Remove(CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == id));
+                CompetitionDB.SaveChanges();
+                return ToJsonOK("Objektas ištrintas.");
+            }
+
+            return ToJsonNotFound("Objektas nerastas.");
+        }
+    }
+}
