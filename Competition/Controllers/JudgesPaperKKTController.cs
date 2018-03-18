e14ebf1c785130e057ce9f96c17b017bd0c236ba
dovged <dovilegedmin@gmail.com>
2018-03-18 16:27:56 +0200

    JudgesPaper KKT controller update

--- Competition/Controllers/JudgesPaperKKTController.cs
+++ Competition/Controllers/JudgesPaperKKTController.cs
@@ -11,46 +11,65 @@ using System.Web.Http;
 
 namespace Competition.Controllers
 {
-    public class JudgesPaperController : BaseAPIController
+    [RoutePrefix("/api/competition/id/judgespapersKKT")]
+    public class JudgesPaperKKTController : BaseAPIController
     {
-        /***/
-        public HttpResponseMessage Get()
+        /** Grąžinamas sąrašas teisėjo lapų pagal varžybas;
+          Surikiuotas pagal Trasas;*/
+        public HttpResponseMessage Get(int compId)
         {
-            if (CompetitionDB.TblJudgesPapers.AsEnumerable() != null)
+            if (CompetitionDB.TblJudgesPapersKKT.AsEnumerable() != null)
             {
-                return ToJson(CompetitionDB.TblJudgesPapers.AsEnumerable());
+                List<RouteKKTModel> routes = CompetitionDB.TblRoutesKKT.ToArray().Where(x => x.CompetitionId == compId).Select(x => new RouteKKTModel(x)).ToList();
+                List<JudgesPaperKKTModel> judgePapers = new List<JudgesPaperKKTModel>();
+
+                foreach (RouteKKTModel r in routes)
+                {
+                    List<JudgesPaperKKTModel> papers = CompetitionDB.TblJudgesPapersKKT.ToArray().Where(x => x.RouteId == r.Id).Select(x => new JudgesPaperKKTModel(x)).ToList();
+
+                    foreach (JudgesPaperKKTModel p in papers)
+                    {
+                        p.RouteName = CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == p.RouteId).Name;
+                        p.TeamName = CompetitionDB.TblTeams.FirstOrDefault(x => x.Id == p.TeamId).Name;
+                        judgePapers.Add(p);
+                    }
+                }
+
+                return ToJsonOK(judgePapers);
             }
-            return Request.CreateResponse(HttpStatusCode.NotFound, "Empty list.");
+
+            return ToJsonNotFound("Tuščias sąrašas.");
         }
 
- 
-        [Authorize]
-        /***/
-        public HttpResponseMessage Get(int id)
+        /** Grąžinamas vienas teisėjo lapas;*/
+        public HttpResponseMessage Get(int compId, int paperId)
         {
-            if (CompetitionDB.TblJudgesPapers.FirstOrDefault(x => x.Id == id) != null)
+            if (CompetitionDB.TblJudgesPapersKKT.FirstOrDefault(x => x.Id == paperId) != null)
             {
-                JudgesPaperKKTModel paper = CompetitionDB.TblJudgesPapers.ToArray().Where(x => x.Id == id).Select(x => new JudgesPaperKKTModel(x)).FirstOrDefault();
-                List<PenaltyQuantityModel> quantity = CompetitionDB.TblPenaltyQuantities.ToArray().Where(x => x.JudgesPaperId == id).Select(x => new PenaltyQuantityModel(x)).ToList();
+                JudgesPaperKKTModel paper = CompetitionDB.TblJudgesPapersKKT.ToArray().Where(x => x.Id == paperId).Select(x => new JudgesPaperKKTModel(x)).FirstOrDefault();
+                List<PenaltyQuantityModel> quantity = CompetitionDB.TblPenaltyQuantities.ToArray().Where(x => x.JudgesPaperId == paperId).Select(x => new PenaltyQuantityModel(x)).ToList();
                 foreach (var q in quantity)
                 {
-                    q.setPenaltyName(CompetitionDB.TblPenalties.FirstOrDefault(x => x.Id == q.PenaltyId).Name.ToString());
+                    q.PenaltyName = CompetitionDB.TblPenalties.FirstOrDefault(x => x.Id == q.PenaltyId).Name.ToString();
                 }
-                paper.SetPenalties(quantity);
-                paper.SetTeamName(CompetitionDB.TblTeams.FirstOrDefault(x => x.Id == paper.TeamId).Name.ToString());
-                paper.SetRouteName(CompetitionDB.TblRoutes.FirstOrDefault(x => x.Id == paper.RouteId).Name.ToString());
-                paper.SetPenalySum(PointsFromPenalties(paper.Id, quantity));
-                paper.SetTimeWithPenalty(SecToTime((TimeToSec(paper.Time)) + (paper.PenaltySum * 30)));
+                paper.Penalties = quantity;
+                paper.TeamName = CompetitionDB.TblTeams.FirstOrDefault(x => x.Id == paper.TeamId).Name.ToString();
+                paper.RouteName = CompetitionDB.TblRoutesKKT.FirstOrDefault(x => x.Id == paper.RouteId).Name.ToString();
+                paper.PenaltySum = PointsFromPenalties(paper.Id, quantity);
+                /** Kaip padaryti dėl skaičiavimo. Baudos verčiamos į laiką ar minusiuojamos nuo taškų*/
+                /***paper.TimeWithPenalty(SecToTime((TimeToSec(paper.Time)) + (paper.PenaltySum * 30)));*/
 
-                return ToJson(paper);
+                return ToJsonOK(paper);
             }
 
-            return Request.CreateResponse(HttpStatusCode.NotFound, "Item not found");
+            return ToJsonNotFound("Objektas nerastas.");
         }
-        [Authorize]
-        /***/
+        
+        /** Sukurti teisėjo lapą*/
         public HttpResponseMessage Post([FromBody]TblJudgesPaperKKT value)
         {
+            /**PRIDĖTI TEISĖJO ID PRIE LAPO*/
+            // TO DO
             /*ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
             string username = identity.Claims.First().Value;
             string accountId = CompetitionDB.Users.FirstOrDefault(x => x.UserName == username).Id.ToString();*/
@@ -58,30 +77,32 @@ namespace Competition.Controllers
             int UserId = Convert.ToInt32(CompetitionDB.TblUsers.FirstOrDefault(x => x.UserId == accountId).Id.ToString());
             if (CompetitionDB.TblCompJuds.FirstOrDefault(x => x.UserId == UserId) != null)
             {*/
-                CompetitionDB.TblJudgesPapers.Add(value);
-                return ToJson(CompetitionDB.SaveChanges());
+            CompetitionDB.TblJudgesPapersKKT.Add(value);
+            CompetitionDB.SaveChanges();
+
+            return ToJsonCreated(value);
            /* }
 
             return Request.CreateResponse(HttpStatusCode.NotFound, "NO access!");*/
         }
 
- 
-        /***/
+        /** Redaguoti teisėjo lapą*/
         public HttpResponseMessage Put(int id, [FromBody]TblJudgesPaperKKT value)
         {
+            /**AR REIKIA TIKRINTI KAS REDAGUOJA???*/
 
-            if (CompetitionDB.TblJudgesPapers.AsEnumerable() != null)
+            if (CompetitionDB.TblJudgesPapersKKT.FirstOrDefault(x => x.Id == id) != null)
             {
                 CompetitionDB.Entry(value).State = EntityState.Modified;
-                return ToJson(CompetitionDB.SaveChanges());
+                CompetitionDB.SaveChanges();
+                return ToJsonOK(value);
             }
 
-            return Request.CreateResponse(HttpStatusCode.NotFound, "Item not found.");
-
+            return ToJsonNotFound("Objektas nerastas.");
         }
         
-        /***/
-        public HttpResponseMessage Delete(int id)
+        /**AR REIKIA IŠTRINTI???*/
+       /* public HttpResponseMessage Delete(int id)
         {
             if (CompetitionDB.TblJudgesPapers.AsEnumerable() != null)
             {
@@ -91,9 +112,9 @@ namespace Competition.Controllers
 
             return Request.CreateResponse(HttpStatusCode.NotFound, "Item not found");
 
-        }
+        }*/
 
-        /***/
+        /** Baudos taškų suma gauna iš visų baudų*/
         public int PointsFromPenalties(int id, List<PenaltyQuantityModel> pen)
         {
             int points = 0;
@@ -104,19 +125,17 @@ namespace Competition.Controllers
                 {
                     if (p != null)
                     {
-                        points += pointSumFromPenalties(p.Id);
+                        points += PointSumFromPenalties(p.Id);
                     }
-
                 }
             }
 
             return points;
         }
 
-        /***/
-        public int pointSumFromPenalties(int id)
+        /** Baudų suma gaunama iš vienos baudų kategorijos*/
+        public int PointSumFromPenalties(int id)
         {
-
             int penaltyId = Convert.ToInt32(CompetitionDB.TblPenaltyQuantities.Find(id).PenaltyId.ToString());
 
             return Convert.ToInt32(CompetitionDB.TblPenaltyQuantities.Find(id).Quantity.ToString()) *
